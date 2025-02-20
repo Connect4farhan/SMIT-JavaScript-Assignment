665b5a03fe3307ad3b8195bb13c0c71ec459a55d
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-20 22:27:13 +0500

    chapter-02.js complete

---
+++ chapter-02.js
@@ -0,0 +1,49 @@
+// // Question-01
+// var username 
+// console.log(username);
+
+
+// // Question-02
+// var myName = "Farhan Ahmed";
+// console.log(myName);
+
+
+// // Question-03
+// var message = "Hello World";
+// alert (message);
+
+
+// // Question-04
+// var studentBioData = { 
+// name: "Jhon Doe",
+// age: "15 years old",
+// course: "certified mobile application Development",}
+// alert(studentBioData.name);
+// alert(studentBioData.age);
+// alert(studentBioData.course);
+
+
+// // Question-05
+// var piza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(piza);
+
+
+// // Question-06
+// var email = "connect4farhanahmed@gmail.com";
+// alert ("My email adress is :" + "  " + email);
+
+
+// // Question-07
+// var book= "'A Smarter Way to Learn Javascript'";
+// alert ("I am trying to learn from the book " + book);
+
+
+// // Question-08
+// document.write("Yah! I can write HTML content through Javascript.") ;
+
+
+// // Question-09 
+// var frame = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert (frame);
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
