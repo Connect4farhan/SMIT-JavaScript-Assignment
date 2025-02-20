25eb01d0e61fcd2d76412e6218114906e0120a90
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-20 23:19:48 +0500

    chapter-03 complete

--- chapter-01.js
+++ chapter-01.js
@@ -1,25 +1,25 @@
-// Question-01
-alert ("wellcome to My Website");
+// // Question-01
+// alert ("wellcome to My Website");
 
 
-// Question-02
-alert ("Error! Please enter a valid password");
+// // Question-02
+// alert ("Error! Please enter a valid password");
 
 
-// Question-03
-alert ("welcome to JS land... \n HAPPY COADING!");
+// // Question-03
+// alert ("welcome to JS land... \n HAPPY COADING!");
 
 
-// Question-04
-alert ("welcome to JS land...");
-alert ("HAPPY COADING!");
-confirm("Prevent this page from creating  additional dialogs");
+// // Question-04
+// alert ("welcome to JS land...");
+// alert ("HAPPY COADING!");
+// confirm("Prevent this page from creating  additional dialogs");
 
 
-// Question-05
-var message = ("Hello... i can run JS through my web browser's console")
-alert (message);
-console.log(message);
+// // Question-05
+// var message = ("Hello... i can run JS through my web browser's console")
+// alert (message);
+// console.log(message);
 
 
 
---
+++ chapter-03.js
@@ -0,0 +1,24 @@
+// Question-01
+var age = 28;
+alert ("I am " + age + " years old");
+
+
+// Question-02
+
+
+
+// Question-03
+var birthYear = 1993;
+document.write("MY birth year is " + birthYear + "<br>");
+document.write("Data Type  of my  decleared variable is " +typeof birthYear);
+
+
+// Question-04
+var xyzClothing = {
+    vsitorName: "jhon Doe" ,
+    quantity: 15,
+    ProductTitle: "XYZ Clothing Store" ,
+}
+alert (xyzClothing.vsitorName + " order " + xyzClothing.quantity + " T-Shirt's On " + xyzClothing.ProductTitle);
+document.write("<br><br>Jhon Doe Ordered 5 T-shirts on XYZ Cloting Store");
+
