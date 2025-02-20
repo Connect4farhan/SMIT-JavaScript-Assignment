ebd8710bf26d856079bda07ec66801037bd739b4
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-21 00:01:58 +0500

    chapter-04.js  git commit -m chapter-04.js

---
+++ chapter-04.js
@@ -0,0 +1,30 @@
+// Question-01
+var firstName = "";
+    secondName = "";
+    fartherNam = "";
+
+// Question-02
+
+//Some examples of valid variable names:
+// 1)Letter
+// 2)Digit
+// 3)Underscore
+// 4)Dollar Sign
+
+// // Some examples of invalid variable names:
+// 1)Hyphens not allowed
+// 2)Can not start with numbers.
+// 3)Spaces not allowed.
+// 4)any other special characters not allowed.
+// 5)Reserved keywords are not allowed.
+
+
+// Question-03
+
+
+document.write("a) Variable name can only contain: Letter,  Digit,  Underscore,  Dollar Sign <br>")
+document.write("b) Vaiable must being with a: $farhan, _farhan, farhan, farhan123 <br>");
+document.write("c) Vaiable name are case sensitive <br>");
+document.write("d) Vaiable names should not  be JS Keyword<br>");  
+
+
