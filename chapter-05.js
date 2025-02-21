4040bb281437d225312b1e7cfefa23a149be07b3
connect4farhan <connect4farhanahmed@gmail.com>
2025-02-22 00:33:36 +0500

    chapter-05 mid compelete

---
+++ chapter-05.js
@@ -0,0 +1,48 @@
+// // Question-01
+
+// var num1 = 5;
+// var num2 = 3;
+// document.write ("sum of 3 and 5 is: ", num1 + num2);
+
+// // Question-02
+
+// // Multipication
+// document.write ("<br>multipication of 3 and 5 is: ", num1 * num2);
+// // Division
+// document.write ("<br>divide of 3 and 5 is: ", num1 - num2);
+// // Subtraction
+// document.write ("<br>subtract of 3 and 5 is: ", num1 / num2);
+// // Modulus
+// document.write ("<br>modulus of 3 and 5 is:", num1 % num2);
+
+// // Question-03
+
+// var Num ;
+// document.write ("<br><br><br><br> value after variable decleartion is: ", Num);
+// Num = 5;
+// document.write ("<br> initial value: ", Num);
+// document.write ("<br> value after increment is: ", ++Num);
+// document.write ("<br> value after addition is: ",Num += 7);
+// document.write ("<br> value after decrement is: ", --Num);
+// document.write ("<br> rimindeer is: ", Num % 3);
+
+
+// // Question-03
+// var ticket = prompt("enter your quaintity");
+// var price = 600
+// document.write ("<br><br><br>  total coast of ticket is: ", ticket * price);
+
+// Question-03
+// var i = 2;
+//         while(i<51)
+// {
+//         if(i%5==0)
+// {
+//         document.write(i + "<br>");
+// }
+//         i++;
+// }
+   
+
+
+
