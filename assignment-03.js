3894053b7bc3099607d1bb5ed9ab69f91ad0081b
connect4farhan <connect4farhanahmed@gmail.com>
2025-03-04 23:21:35 +0500

    1st commit

---
+++ assignment-03.js
@@ -0,0 +1,146 @@
+var products = [
+  {
+    id: 101,
+    title: "Sony LED 40 inch",
+    variations: [
+      { id: 1, color: "black", price: 50000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+      { id: 3, color: "silver", price: 55000, quantity: 8 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 4.5,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+      {
+        id: 3,
+        user: "Ali",
+        rating: 5.0,
+        title: "bad Product",
+        comments: "ali It is a very good product ....",
+        date: "04-02-2021",
+        status: true,
+      },
+    ],
+  },
+  {
+    id: 102,
+    title: "Mobile",
+    variations: [
+      { id: 1, color: "black", price: 50000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+      { id: 3, color: "silver", price: 55000, quantity: 8 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 4.5,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+      {
+        id: 3,
+        user: "Ali",
+        rating: 5.0,
+        title: "bad Product",
+        comments: "ali It is a very good product ....",
+        date: "04-02-2021",
+        status: true,
+      },
+    ],
+  },
+  {
+    id: 103,
+    title: "Bike",
+    variations: [
+      { id: 1, color: "black", price: 55000, quantity: 5 },
+      { id: 2, color: "red", price: 50000, quantity: 1 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        user: "Ahmad",
+        rating: 4.0,
+        title: "Good Product",
+        comments: "It is a very good product ....",
+        date: "06-02-2021",
+        status: true,
+      },
+      {
+        id: 2,
+        user: "Zubair",
+        rating: 3.0,
+        title: "Very Good Product",
+        comments: "zubair It is a very good product ....",
+        date: "05-02-2021",
+        status: false,
+      },
+    ],
+  },
+];
+
+//----------------------------------------------------------------------------
+// Question-01  Find a Product by ID ?
+//Write a program that takes a product ID and prints the product object.
+
+//console.log(products[1]);
+
+
+// Question-02  List All Product Titles
+//Write a program that prints all product titles.
+
+
+// for (var i = 0; i < products.length; i++) {
+//   var product = products[i];
+//   console.log(product.title);
+// }
+
+
+// Question-03  Find Available Colors of a Product
+//Given a product ID, print all available colors.
+
+for (var i = 0; i < products.length; i++) {
+  var color = products[i];
+ 
+}
+console.log(products.id[0]);
+
+// Question-03  Get Total Quantity of a Product
+//Write a program that calculates and prints the total quantity of all variations of a given product.
+
+var totalQuantity = 0;
+for (var i = 0; i < products.variations.length; i++) {
+  console.log(products.variations[i])
+  totalQuantity = totalQuantity + products.variations[i].quantity;
+}
+
+// var avg = totalQuantity / product.variations.length;
+
+console.log(totalQuantity);
+console.log(avg);
